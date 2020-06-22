d2e328dfa57541b588617b2601ab6af428a439db
nemo-omen <nemo.omen@gmail.com>
2020-06-21 23:38:09 -0500

    Very basic components & layout

---
+++ components/post-list-item.js
@@ -0,0 +1,98 @@
+import { LitElement, html, css } from 'lit-element';
+
+class PostListItem extends LitElement {
+  static get properties() {
+    return {
+      postid: { type: String },
+      headline: { type: String },
+      byline: { type: String },
+      dateline: { type: String },
+      lastUpdated: { type: String },
+      featuredImage: { type: String },
+      content: { type: String },
+      slug: { type: String },
+      permalink: { type: String }
+    }
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        cursor: pointer;
+      }
+      article {
+        font-family: 'IBM Plex Sans';
+        display: flex;
+        flex-direction: row;
+        padding: 0 1rem 0 0;
+        background-color: #1f2329;
+        transition-property: all;
+        transition-duration: 400ms;
+      }
+      article:hover h2 {
+        text-decoration: underline;
+      }
+      article:hover h3, article:hover p {
+        color: #fafafa;
+      }
+      img {
+        max-width: 300px;
+      }
+      .main-section {
+        margin: 0 0 0 1rem;
+        padding: 0;
+        flex-grow: 1;
+      }
+      header {
+        border-bottom: 1px dotted #ffc200;
+        display: flex;
+        flex-direction: column;
+      }
+      .subheadline-section {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        border-top: 1px dotted #ffc200;
+        padding: 0.25rem 0;
+      }
+      h2 {
+        margin: 0 0 0.5rem 0;
+        padding: 0;
+        font-weight: 300;
+        font-size: 1.75rem;
+        color: #0dffe4;
+        text-decoration: none;
+        transition-property: all;
+        transition-duration: 400ms;
+      }
+      h3 {
+        padding: 0;
+        margin: 0;
+        font-weight: 400;
+        font-size: 1rem;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+    <article>
+        <img src=${this.featuredImage} alt="${this.headline}">
+      <section class="main-section">
+        <header>
+          <h2>${this.headline}</h2>
+          <section class="subheadline-section">
+            <h3>by ${this.byline}</h3>
+            <h3>${this.dateline}</h3>
+          </section>
+        </header>
+        <section class="content-section">
+          <p>${this.content}</p>
+        </section>
+      </section>
+    </article>
+    `;
+  }
+}
+
+customElements.define('post-list-item', PostListItem);
--- components/post-list.js
+++ components/post-list.js
@@ -1,6 +1,29 @@
 import { LitElement, html, css } from 'lit-element';
+import './post-list-item.js';
+
+const dummyPosts = [
+  {
+    postid: '1',
+    headline: 'Post one headline: Important things afoot at City Council',
+    byline: 'El Zanate Staff',
+    dateline: 'June 21, 2020, 9:35 p.m.',
+    lastUpdated: 'June 21, 2020, 9:35 p.m.',
+    featuredImage: '/assets/images/Great-tailed_Grackle_67332870.jpg',
+    content: 'Something really big and important happened at 9:30 this evening, Sunday, June 21, 2020.'
+  }
+];
 
 class PostList extends LitElement {
+  static get properties() {
+    return {
+      posts: { type: Array }
+    }
+  }
+
+  constructor() {
+    super();
+    this.posts = [...dummyPosts];
+  }
 
   static get styles() {
     return css`
@@ -8,15 +31,36 @@ class PostList extends LitElement {
         color: #0dffe4;
         font-weight: 200;
         font-size: 2rem;
+        padding: 0;
+        margin: 0;
+      }
+      a {
+        text-decoration: none;
+        color: inherit;
       }
     `;
   }
+
   render() {
     return html`
-      <h1>Posts</h1>
-      <p>More stuff here.</p>
-    `;
+      ${this.posts.map(post => html`
+      <a href="/">
+        <post-list-item
+          id=${post.postid}
+          .postid=${post.postid}
+          .headline=${post.headline}
+          .byline=${post.byline}
+          .dateline=${post.dateline}
+          .lastUpdated=${post.lastUpdated}
+          .featuredImage=${post.featuredImage}
+          .content=${post.content}
+        >
+        </post-list-item>
+        </a>
+      `)}
+    `
   }
+
 }
 
 customElements.define('post-list', PostList);
