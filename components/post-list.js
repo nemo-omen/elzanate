fe6c9290f5a5384d532c0323f8080a420de7a85a
Jeff Caldwell <nemo.omen@gmail.com>
2020-06-25 23:54:12 -0500

    Added not-yet-really-functional express server

--- components/post-list.js
+++ components/post-list.js
@@ -1,35 +1,6 @@
 import { LitElement, html, css } from 'lit-element';
+import { until } from 'lit-html/directives/until.js';
 import './post-list-item.js';
-
-const dummyPosts = [
-  {
-    postid: '1',
-    headline: 'Post one headline: Important things afoot at City Council',
-    byline: 'El Zanate Staff',
-    dateline: 'June 21, 2020, 9:35 p.m.',
-    lastUpdated: 'June 21, 2020, 9:35 p.m.',
-    featuredImage: '/assets/images/Great-tailed_Grackle_67332870.jpg',
-    content: 'Something really big and important happened at 9:30 this evening, Sunday, June 21, 2020.'
-  },
-  {
-    postid: '2',
-    headline: 'What do the improvements to the Lake Nasworthy area mean for you?',
-    byline: 'El Zanate Staff',
-    dateline: 'June 21, 2020, 11:40 p.m.',
-    lastUpdated: 'June 21, 2020, 11:40 p.m.',
-    featuredImage: '/assets/images/Great-tailed_Grackle_National_Butterfly_Center_Mission_TX_2018-02-28_15-40-11_(38852714480).jpg',
-    content: 'Do the improvements to the residential areas of Lake Nasworthy mean anything for you if you live in Blackshear?'
-  },
-  {
-    postid: '3',
-    headline: "The largest property owner in Downtown San Angelo doesn't pay property taxes",
-    byline: 'Jeff Caldwell',
-    dateline: 'June 22, 2020, 9:09 p.m.',
-    lastUpdated: 'June 22, 2020, 9:09 p.m.',
-    featuredImage: '/assets/images/Great-tailed_Grackle_From_The_Crossley_ID_Guide_Eastern_Birds.jpg',
-    content: 'How the city loses out on millions in revenue from one of its largest property owners.'
-  }
-];
 
 class PostList extends LitElement {
   static get properties() {
@@ -40,7 +11,7 @@ class PostList extends LitElement {
 
   constructor() {
     super();
-    this.posts = [...dummyPosts];
+    this.posts = [];
   }
 
   static get styles() {
@@ -57,21 +28,32 @@ class PostList extends LitElement {
 
   render() {
     return html`
-      ${this.posts.map(post => html`
-        <post-list-item
-          id=${post.postid}
-          .postid=${post.postid}
-          .headline=${post.headline}
-          .byline=${post.byline}
-          .dateline=${post.dateline}
-          .lastUpdated=${post.lastUpdated}
-          .featuredImage=${post.featuredImage}
-          .content=${post.content}
-        >
-        </post-list-item>
-      `)}
-    `
+    ${
+      until(
+        fetch('http://localhost:3000')
+          .then(response => response.json())
+          .then((data) => { this.posts = [...data] })
+          .catch((error) => { console.error(error) }),
+
+        html`
+          ${this.posts.map(post => html`
+          <post-list-item
+            id=${post.postid}
+            .postid=${post.postid}
+            .headline=${post.headline}
+            .byline=${post.byline}
+            .dateline=${post.dateline}
+            .lastUpdated=${post.lastUpdated}
+            .featuredImage=${post.featuredImage}
+            .content=${post.content}
+          >
+          </post-list-item>
+        `)}
+        `
+      )
+      }`
   }
+
 
 }
 
---
+++ server/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const nano = require('nano')('http://192.168.0.11:5984');
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+// nano.db.create('ezposts');
+const posts = nano.db.use('ezposts');
+
+const dummyPosts = [
+	{
+		postid: '1',
+		headline: 'Post one headline: Important things afoot at City Council',
+		byline: 'El Zanate Staff',
+		dateline: 'June 21, 2020, 9:35 p.m.',
+		lastUpdated: 'June 21, 2020, 9:35 p.m.',
+		featuredImage: '/assets/images/Great-tailed_Grackle_67332870.jpg',
+		content: 'Something really big and important happened at 9:30 this evening, Sunday, June 21, 2020.'
+	},
+	{
+		postid: '2',
+		headline: 'What do the improvements to the Lake Nasworthy area mean for you?',
+		byline: 'El Zanate Staff',
+		dateline: 'June 21, 2020, 11:40 p.m.',
+		lastUpdated: 'June 21, 2020, 11:40 p.m.',
+		featuredImage: '/assets/images/Great-tailed_Grackle_National_Butterfly_Center_Mission_TX_2018-02-28_15-40-11_(38852714480).jpg',
+		content: 'Do the improvements to the residential areas of Lake Nasworthy mean anything for you if you live in Blackshear?'
+	},
+	{
+		postid: '3',
+		headline: "The largest property owner in Downtown San Angelo doesn't pay property taxes",
+		byline: 'Jeff Caldwell',
+		dateline: 'June 22, 2020, 9:09 p.m.',
+		lastUpdated: 'June 22, 2020, 9:09 p.m.',
+		featuredImage: '/assets/images/Great-tailed_Grackle_From_The_Crossley_ID_Guide_Eastern_Birds.jpg',
+		content: 'How the city loses out on millions in revenue from one of its largest property owners.'
+	}
+];
+
+app.get('/', (req, res) => {
+	// res.send('You are connected.');
+	res.json(dummyPosts);
+});
+
+app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
